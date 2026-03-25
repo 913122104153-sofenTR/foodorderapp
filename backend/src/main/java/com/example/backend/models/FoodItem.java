49e7d4d4ab8af46993d1ca96763ba63c19de85ff
913122104153-sofenTR <trsofen2003@gmail.com>
2026-03-25 12:03:10 +0530

    restaurant controller

---
+++ backend/src/main/java/com/example/backend/models/FoodItem.java
@@ -0,0 +1,22 @@
+package com.example.backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
